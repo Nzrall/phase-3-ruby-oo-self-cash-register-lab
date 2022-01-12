b43f73c1b6b3ef89f624113678858b75bb1db401
Joshua <wilson.joshua@outlook.com>
2022-01-12 15:41:08 -0800

    commit tree e9fbb3ef39d55f67d5312a5c5746e881d1437f40

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,14 @@
+class CashRegister
+    attr_accessor :total, :discount, :items, :last_transaction
 
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+
+    def add_item(title, price)
+    
+    end
+
+end
